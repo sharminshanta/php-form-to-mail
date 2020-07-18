e12555509b56f84854f4174f21d36a3099b3be9c
sharminex81 <mimetyshanta@gmail.com>
2020-07-18 18:34:00 +0600

    index file has been added.

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Email Template | @yield('title')</title>
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/jquery.min.js"></script>
+    <!------ Include the above in your HEAD tag ---------->
+
+</head>
+<body>
+<div class="container">
+    <div class="mt-5">
+
+    </div>
+</div>
+</body>
+</html>
